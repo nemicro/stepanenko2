1d1c5eb648fb2b612efbbca0487baff35d6e3184
nemicro <vlad.vsmail@gmail.com>
2017-09-07 23:27:49 +0300

    Готово. Практически готово :)

--- exam2/main.js
+++ exam2/main.js
@@ -1,30 +1,55 @@
 ;(function ($) {
     $(function () {
         $('.team-slider, .testimonials-slider').slick({
-            arrows:false,
-            dots:true,
-            dotsClass:"team-slider-dots",
-            autoplay:true,
-            autoplaySpeed:2000
+            arrows: false,
+            dots: true,
+            dotsClass: "team-slider-dots",
+            autoPlay: true
         });
     });
-    var $grid = $('.grid').isotope({
-        itemSelector: '.element-item',
-        layoutMode: 'fitRows',
-        getSortData: {
-            AllWorks: '[data-AllWorks]',
-            Print: '[data-Print]',
-            Branding: '[data-Branding]',
-            Web: '[data-Web]'
-        }
-    });
-
-
-    $('#sorts').on( 'click', 'button', function() {
-        var sortByValue = $(this).attr('data-sort-by');
-        $grid.isotope({
-            sortBy: sortByValue,
-            sortAscending:false
+    $(function () {
+        var map,
+            point = {lat: 48.734326, lng: 37.579426};
+        map = new google.maps.Map(document.getElementById('googleMap'), {
+            center: point,
+            zoom: 15
         });
+        var img = new google.maps.MarkerImage('marker.png');
+        var marker = new google.maps.Marker({
+            position: point,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: img
+        });
+
     });
-})(jQuery);
+    $('.iso-main-in-portfolio').isotope({
+        itemSelector: '.item'
+    });
+    $('.iso-nav-in-portfolio div').click(function () {
+        $('.iso-nav-in-portfolio div').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        $('.iso-main-in-portfolio').isotope({
+            filter: selector
+        });
+
+    });
+    $('.menu').on('click','a', function () {
+       event.preventDefault();
+       var id = $(this).attr('href'),
+           top = $(id).offset().top;
+       $('body,html').animate({scrollTop:top}, 500)
+    });
+    $('.scroll-button-in-header').on('click','a', function () {
+       event.preventDefault();
+       var id = $(this).attr('href'),
+           top = $(id).offset().top;
+       $('body,html').animate({scrollTop:top}, 500)
+    });
+    $('.menu-button1').click(function () {
+        $('body').append('<audio autoplay controls  src="sound/adel_-_hello_(zf.fm).mp3"></audio>')
+
+    });
+
+    })(jQuery);
